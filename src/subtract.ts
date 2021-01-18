769e13cdde6fe807ea37c5f1ebfe06e1d98bcd39
Nasim Uddin <ficklenasim@outlook.com>
2021-01-18 17:39:22 +0600

    api-extractor basic setup

---
+++ src/subtract.ts
@@ -0,0 +1,11 @@
+/**
+ * Subtract to number
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @beta
+ */
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
